f3231dab3c07643811d398e00375cb666918826b
pranavm7 <pranavm7@outlook.com>
2023-11-15 16:41:19 -0500

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray':'#B5C0BC',
+        'custom-green':'#0D575F',
+        'custom-blue':'#273654',
+      }
+    },
   },
   plugins: [],
 };
