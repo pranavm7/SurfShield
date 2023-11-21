a5b7db7635e2a35536be9518bea9107fe13b135b
pranavm7 <pranavm7@outlook.com>
2023-11-21 00:51:22 -0500

    [Bug] : Tailwind not active at Options.tsx

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import '@pages/options/Options.css';
 
 const Options: React.FC = () => {
-  return <div className="container">Options</div>;
+  return (
+    <div className="w-full h-full text-center flex bg-custom-blue ">
+      <h1 className="text-custom-white">Advanced Metrics (Coming Soon)</h1>
+    </div>
+  );
 };
 
 export default Options;
